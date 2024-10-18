c86da6f7dedbeaed75c395ec82eacdc9fe91e9cb
Oliver Säfström <oliver.safstrom@mobilityguard.com>
2024-10-18 12:48:14 +0200

    Initial commit adding some basic crates

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use dotenv::dotenv;
+
+extern crate dotenv;
+extern crate pretty_env_logger;
+
+#[macro_use]
+extern crate log;
+
+fn main() {
+    dotenv().ok();
+    pretty_env_logger::init();
+    info!("Hello");
+}
